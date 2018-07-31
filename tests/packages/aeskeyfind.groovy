684c6bca6be17ea5121da5e61215f58eaa866cc0
Erik Kristensen <erik@erikkristensen.com>
2018-07-30 19:00:24 -0600

    saving aeskeyfind

--- tests/packages/aeskeyfind.groovy
+++ tests/packages/aeskeyfind.groovy
@@ -3,27 +3,30 @@ pipelineJob('aeskeyfind') {
     cps {
       sandbox()
       script("""
-        node('docker') {
-            stage('Prepare') {
-                steps {
-                    container('dind') {
-                        sh """
-                        docker pull sansdfir/sift-salt-tester
-                        """
+        pipeline {
+            agent none
+            stages {
+                stage('Clone') {
+                    steps {
+                        git "https://github.com/sans-dfir/sift-saltstack.git"
                     }
                 }
-            }
-            stage('Clone') {
-                steps {
-                    git "https://github.com/sans-dfir/sift-saltstack.git"
+                stage('Prepare') {
+                    steps {
+                        node('docker') {
+                          container('dind') {
+                              sh "docker pull sansdfir/sift-salt-tester"
+                          }
+                        }
+                    }
                 }
-            }
-            stage('Test - aeskeyfind') {
-                steps {
-                    container('dind') {
-                        sh """
-                        docker run --rm --cap-add SYS_ADMIN -v /Users/ekristen/Development/sift/sift-jenkins/sift:/srv/salt/sift sansdfir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.aeskeyfind
-                        """
+                stage('Test') {
+                    steps {
+                        node('docker') {
+                          container('dind') {
+                              sh "docker run --rm --cap-add SYS_ADMIN -v /Users/ekristen/Development/sift/sift-jenkins/sift:/srv/salt/sift sansdfir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.aeskeyfind"
+                          }
+                        }
                     }
                 }
             }
