f9ec364f2128ac862131cfbb94cd1ed1502088fb
Erik Kristensen <erik@erikkristensen.com>
2018-07-30 18:58:20 -0600

    saving aeskeyfind

--- tests/packages/aeskeyfind.groovy
+++ tests/packages/aeskeyfind.groovy
@@ -1,37 +1,29 @@
-
 pipelineJob('aeskeyfind') {
   definition {
     cps {
       sandbox()
       script("""
-        pipeline {
-            agent {
-                node {
-                    label 'docker'
+        node('docker') {
+            stage('Prepare') {
+                steps {
+                    container('dind') {
+                        sh """
+                        docker pull sansdfir/sift-salt-tester
+                        """
+                    }
                 }
             }
-            stages {
-                stage('Prepare') {
-                    steps {
-                        container('dind') {
-                            sh """
-                            docker pull sansdfir/sift-salt-tester
-                            """
-                        }
-                    }
+            stage('Clone') {
+                steps {
+                    git "https://github.com/sans-dfir/sift-saltstack.git"
                 }
-                stage('Clone') {
-                    steps {
-                        git "https://github.com/sans-dfir/sift-saltstack.git"
-                    }
-                }
-                stage('Test - aeskeyfind') {
-                    steps {
-                        container('dind') {
-                            sh """
-                            docker run --rm --cap-add SYS_ADMIN -v /Users/ekristen/Development/sift/sift-jenkins/sift:/srv/salt/sift sansdfir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.aeskeyfind
-                            """
-                        }
+            }
+            stage('Test - aeskeyfind') {
+                steps {
+                    container('dind') {
+                        sh """
+                        docker run --rm --cap-add SYS_ADMIN -v /Users/ekristen/Development/sift/sift-jenkins/sift:/srv/salt/sift sansdfir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.aeskeyfind
+                        """
                     }
                 }
             }
