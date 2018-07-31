0edd4eb9aa4efce7f4a13eb329d1d8145e16cabb
Erik Kristensen <erik@erikkristensen.com>
2018-07-30 19:30:06 -0600

    make it work

--- tests/packages/afflib_tools.groovy
+++ tests/packages/afflib_tools.groovy
@@ -5,30 +5,28 @@ pipelineJob('sift/packages/afflib_tools') {
       sandbox()
       script("""
         pipeline {
-            agent none
+            agent {
+              node {
+                label 'docker'
+              }
+            }
             stages {
                 stage('Clone') {
                     steps {
-                        node('docker') {
-                            git "https://github.com/sans-dfir/sift-saltstack.git"
-                        }
+                        git "https://github.com/sans-dfir/sift-saltstack.git"
                     }
                 }
                 stage('Fetch Docker Testing Image') {
                     steps {
-                        node('docker') {
-                            container('dind') {
-                                sh "docker pull sansdfir/sift-salt-tester"
-                            }
+                        container('dind') {
+                            sh "docker pull sansdfir/sift-salt-tester"
                         }
                     }
                 }
                 stage('Test State') {
                     steps {
-                        node('docker') {
-                            container('dind') {
-                                sh "docker run --rm --cap-add SYS_ADMIN -v `pwd`/sift:/srv/salt/sift sansdfir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.afflib-tools"
-                            }
+                        container('dind') {
+                            sh "docker run --rm --cap-add SYS_ADMIN -v `pwd`/sift:/srv/salt/sift sansdfir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.afflib-tools"
                         }
                     }
                 }
