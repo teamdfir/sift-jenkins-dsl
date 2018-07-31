2499ce9d19892a9095a7c81bc4b10f6132c0ec7d
Erik Kristensen <erik@erikkristensen.com>
2018-07-30 19:53:11 -0600

    making it work

--- tests/packages/cryptcat.groovy
+++ tests/packages/cryptcat.groovy
@@ -1,28 +1,5 @@
-#!/bin/bash
 
-# This script generates the DSLs for building Dockerfiles in each of my repos.
-
-set -e
-set -o pipefail
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
-
-generate_dsl(){
-	local orig=$1
-	local name=${orig#*/}
-
-	local image=${name}
-  
-  name=$(basename $name)
-
-  rname=${name//.sls/}
-	file="${DIR}/tests/packages/${rname//[.-]/_}.groovy"
-
-	echo "-  ${file} == ${rname}"
-
-	cat <<-EOF > $file
-
-pipelineJob('sift/packages/${rname//[.-]/_}') {
+pipelineJob('sift/packages/cryptcat') {
   definition {
     cps {
       sandbox()
@@ -49,7 +26,7 @@ pipelineJob('sift/packages/${rname//[.-]/_}') {
                 stage('Test State') {
                     steps {
                         container('dind') {
-                            sh "docker run --rm --cap-add SYS_ADMIN -v \`pwd\`/sift:/srv/salt/sift sansdfir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.$rname"
+                            sh "docker run --rm --cap-add SYS_ADMIN -v `pwd`/sift:/srv/salt/sift sansdfir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.cryptcat"
                         }
                     }
                 }
@@ -68,19 +45,3 @@ pipelineJob('sift/packages/${rname//[.-]/_}') {
       daysToKeep(15)
   }
 }
-EOF
-
-}
-
-packages=$(ls ../sift-saltstack/sift/packages/*.sls | head -n 30)
-
-main(){
-	mkdir -p $DIR/tests/packages
-	echo "FILE | IMAGE"
-
-	for r in $packages; do
-    generate_dsl "${r}"
-	done
-}
-
-main
