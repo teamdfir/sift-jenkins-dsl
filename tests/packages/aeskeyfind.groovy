c9b774217c60e65659468d8382b332a68b303175
Erik Kristensen <erik@erikkristensen.com>
2018-07-30 18:48:37 -0600

    saving aeskeyfind

--- tests/packages/aeskeyfind.groovy
+++ tests/packages/aeskeyfind.groovy
@@ -1,63 +1,42 @@
-freeStyleJob('aeskeyfind') {
-    displayName('aeskeyfind.sls')
-    description('Test SLS file in aeskeyfind.sls.')
 
-    concurrentBuild()
-    checkoutRetryCount(3)
-
-    properties {
-        githubProjectUrl('https://github.com/sans-dfir/sift-saltstack')
-    }
-
-    logRotator {
-        numToKeep(100)
-        daysToKeep(15)
-    }
-
-    scm {
-        git {
-            remote {
-                url('https://github.com/sans-dfir/sift-saltstack.git')
+pipelineJob('aeskeyfind') {
+  definition {
+    cps {
+      sandbox()
+      script("""
+        pipeline {
+            agent {
+                node {
+                    label 'docker'
+                }
             }
-      branches('*/master', '*/tags/*')
-            extensions {
-                wipeOutWorkspace()
-                cleanAfterCheckout()
-            }
-        }
-    }
-
-    triggers {
-        cron('H H * * *')
-        githubPush()
-    }
-
-    wrappers { colorizeOutput() }
-
-    environmentVariables(DOCKER_CONTENT_TRUST: '1')
-    steps {
-        shell('export BRANCH=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match || echo "master"); if [[ "$BRANCH" == "master" ]]; then export BRANCH="latest"; fi; echo "$BRANCH" > .branch')
-        shell('docker pull sansdfir/sift-salt-tester')
-        shell('docker run --it --rm --cap-add SYS_ADMIN sansdir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.aeskeyfind')
-
-    }
-
-    publishers {
-        retryBuild {
-            retryLimit(2)
-            fixedDelay(15)
-        }
-
-        extendedEmail {
-            recipientList('$DEFAULT_RECIPIENTS')
-            contentType('text/plain')
-            triggers {
-                stillFailing {
-                    attachBuildLog(true)
+            stages {
+                stage('Prepare') {
+                    steps {
+                        container('dind') {
+                            sh """
+                            docker pull sansdfir/sift-salt-tester
+                            """
+                        }
+                    }
+                }
+                stage('Clone') {
+                    steps {
+                        git "https://github.com/sans-dfir/sift-saltstack.git"
+                    }
+                }
+                stage('Test - aeskeyfind') {
+                    steps {
+                        container('dind') {
+                            sh """
+                            docker run --rm --cap-add SYS_ADMIN -v /Users/ekristen/Development/sift/sift-jenkins/sift:/srv/salt/sift sansdfir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.aeskeyfind
+                            """
+                        }
+                    }
                 }
             }
         }
-
-        wsCleanup()
+      """.stripIndent())
     }
+  }
 }
