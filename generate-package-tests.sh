89e559b4052965600b87058b8cc3833ca2b7e872
Erik Kristensen <erik@erikkristensen.com>
2019-07-18 09:09:57 -0600

    changes

--- generate-package-tests.sh
+++ generate-package-tests.sh
@@ -9,7 +9,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
 generate_dsl(){
   local prefix=${1}
-	local orig=$2
+	local orig=${2}
+  local distro=${3}
 	local name=${orig#*/}
 
 	local image=${name}
@@ -17,13 +18,13 @@ generate_dsl(){
   name=$(basename $name)
 
   rname=${name//.sls/}
-	file="${DIR}/saltstack/${prefix//-/_}_${rname//[\+.-]/_}.groovy"
+	file="${DIR}/${distro}/${prefix//-/_}_${rname//[\+.-]/_}.groovy"
 
 	echo "-  ${file} == ${rname}"
 
 	cat <<-EOF > $file
 
-pipelineJob('sift/saltstack/${rname//[\+.-]/_}') {
+pipelineJob('sift/${distro}/${rname//[\+.-]/_}') {
   definition {
     cps {
       sandbox()
@@ -43,14 +44,14 @@ pipelineJob('sift/saltstack/${rname//[\+.-]/_}') {
                 stage('Fetch Testing Image') {
                     steps {
                         container('dind') {
-                            sh "docker pull sansdfir/sift-salt-tester"
+                            sh "docker pull sansdfir/sift-salt-tester:${distro}"
                         }
                     }
                 }
                 stage('Test State') {
                     steps {
                         container('dind') {
-                            sh "docker run --rm --cap-add SYS_ADMIN -v \`pwd\`/sift:/srv/salt/sift sansdfir/sift-salt-tester salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.$prefix.$rname"
+                            sh "docker run --rm --cap-add SYS_ADMIN -v \`pwd\`/sift:/srv/salt/sift sansdfir/sift-salt-tester:${distro} salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.$prefix.$rname"
                         }
                     }
                 }
@@ -90,22 +91,28 @@ packages=$(ls ../sift-saltstack/sift/packages/*.sls | grep -v init.sls)
 python_packages=$(ls ../sift-saltstack/sift/python-packages/*.sls | grep -v init.sls)
 scripts=$(ls ../sift-saltstack/sift/scripts/*.sls | grep -v init.sls)
 
+distros="bionic
+xenial"
+
 main(){
-	mkdir -p $DIR/saltstack
-  rm -f $DIR/saltstack/*.groovy
-	echo "FILE | IMAGE"
+  for d in $distros; do
+  	mkdir -p $DIR/$d
+    rm -f $DIR/$d/*.groovy
+  	echo "FILE | IMAGE"
 
-	for r in $packages; do
-    generate_dsl "packages" "${r}"
-	done
+    for r in $packages; do
+      generate_dsl "packages" "${r}" "${d}"
+    done
 
-	for r in $python_packages; do
-    generate_dsl "python-packages" "${r}"
-	done
+    for r in $python_packages; do
+      generate_dsl "python-packages" "${r}" "${d}"
+    done
 
-	for r in $scripts; do
-    generate_dsl "scripts" "${r}"
-	done
+    for r in $scripts; do
+      generate_dsl "scripts" "${r}" "${d}"
+    done
+
+  done
 }
 
 main
---
+++ xenial/packages_arc_icons.groovy
@@ -0,0 +1,60 @@
+
+pipelineJob('sift/xenial/arc_icons') {
+  definition {
+    cps {
+      sandbox()
+      script("""
+        pipeline {
+            agent {
+              node {
+                label 'docker'
+              }
+            }
+            stages {
+                stage('Clone') {
+                    steps {
+                        git "https://github.com/sans-dfir/sift-saltstack.git"
+                    }
+                }
+                stage('Fetch Testing Image') {
+                    steps {
+                        container('dind') {
+                            sh "docker pull sansdfir/sift-salt-tester:xenial"
+                        }
+                    }
+                }
+                stage('Test State') {
+                    steps {
+                        container('dind') {
+                            sh "docker run --rm --cap-add SYS_ADMIN -v `pwd`/sift:/srv/salt/sift sansdfir/sift-salt-tester:xenial salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.packages.arc-icons"
+                        }
+                    }
+                }
+            }
+        }
+      """.stripIndent())
+    }
+  }
+
+  triggers {
+      cron('W W * * *')
+  }
+
+  logRotator {
+      numToKeep(100)
+      daysToKeep(15)
+  }
+  
+  publishers {
+      extendedEmail {
+          recipientList('')
+          contentType('text/plain')
+          triggers {
+              stillFailing {
+                    attachBuildLog(true)
+              }
+          }
+      }
+      wsCleanup()
+  }
+}
